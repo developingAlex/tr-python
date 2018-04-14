f17eed05ebc2726f8b2257fb904d5fac9c318a8a
Alex Palma <alex-palma@outlook.com>
2018-04-14 22:21:55 +1000

    toyrobot.py: Add place method
    This is the method that is called when you want to place
    the robot on the table

--- toyrobot.py
+++ toyrobot.py
@@ -3,13 +3,31 @@
 
 class Robot:
   """The toy robot class"""
-  facing = 'N'
+  compass = ['N','E','S','W']
 
-  def __init__(self, name):
+  def __init__(self, name, table_height, table_width):
     self.name = name
     self.facing = None
     self.x = None
     self.y = None
+    self.table_height = table_height
+    self.table_width = table_width
+  
+  def place(x,y,facing):
+    if x >= table_width or y >= table_height:
+      print("invalid placement")
+      return false
+    self.x = x
+    self.y = y
+
+    if facing in compass:
+      self.facing = facing
+    else:
+      self.facing = 'N'
+      
+    return true
+  
+  
 
 robot = Robot('alex')
 print(robot.name)
