fab826c7fe512da88e68e13f94c9f8dc343f7a78
Alex Palma <alex-palma@outlook.com>
2018-04-15 00:03:34 +1000

    test_toyrobot.py: Test robots properties at initialisation

--- test_toyrobot.py
+++ test_toyrobot.py
@@ -13,8 +13,17 @@ class RobotTestCase(unittest.TestCase):
   def setUp(self):
     self.robot = Robot('Testy', 5, 5)
   
-  def testPlacement(self):
-    self.robot.place(4,4,'S')
+  def testSetUp(self):
+    self.assertEqual(self.robot.name, 'Testy')
+    self.assertEqual(self.robot.table_height, 5)
+    self.assertEqual(self.robot.table_width, 5)
+
+  def testValidPlacement(self):
+    # if the placement is allowed, place returns True
+    self.assertTrue(self.robot.place(0,4,'S'))
+    self.assertTrue(self.robot.place(4,4,'S'))
+    # go ahead and test the properties match our parameters
     self.assertEqual(self.robot.facing, 'S')
     self.assertEqual(self.robot.x, 4)
     self.assertEqual(self.robot.y, 4)
+  
